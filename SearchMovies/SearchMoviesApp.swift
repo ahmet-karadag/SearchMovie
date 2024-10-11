b183b2565526e3b3a3a9ebab2ccf65dc7cca1c2e
Ahmet Karadağ <akkaradaga@gmail.com>
2024-10-11 10:51:39 +0300

    Initial Commit

---
+++ SearchMovies/SearchMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchMoviesApp.swift
+//  SearchMovies
+//
+//  Created by ahmet karadağ on 11.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct SearchMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
